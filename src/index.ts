c84e6acadbf19f76e0dfef6e4bfe95848b34e4ac
ifs-shiflk <shadiya.iffath@ifsworld.com>
2023-05-21 01:07:32 +0530

    Complete express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import http from 'http'
+
+const app = express();
+
+app.use(cors({
+    credentials:true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log(`Server running on localhost port 8080`)
+});    
