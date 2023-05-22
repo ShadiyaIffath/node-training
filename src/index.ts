f9a9b9061f9d1238869ed6ae5aad0a8047726b08
ifs-shiflk <shadiya.iffath@ifsworld.com>
2023-05-22 22:13:02 +0530

    setting up mongoose and env setup done

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,37 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import compression from 'compression'
-import cors from 'cors'
-import http from 'http'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import http from 'http';
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT;
+const mongoURL = process.env.MONGODB;
 
-app.use(cors({
-    credentials:true
-}))
+app.use(
+	cors({
+		credentials: true,
+	})
+);
 
-app.use(compression())
-app.use(cookieParser())
+app.use(compression());
+app.use(cookieParser());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log(`Server running on localhost port 8080`)
-});    
+server.listen(port, () => {
+	console.log(`Server running on localhost port ${port}`);
+});
+
+try {
+    mongoose.connect(mongoURL);
+    console.log('Database connection successful.')
+  } catch (error) {
+    console.log(`error occured: ${error}`)
+  }
+mongoose.connection.on('connection', (stream) => {
+	console.log('Connection established with database');
+});
